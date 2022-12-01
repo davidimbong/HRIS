a22996986c92c6efc21ef3835fe86d7defcfba91
davidimbong <davidimbong.it@gmail.com>
2022-12-01 16:24:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hris/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hris.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hris.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/hris/ui/fragments/ProfileFragment.kt
@@ -0,0 +1,32 @@
+package com.example.hris.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.hris.R
+import com.example.hris.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnUpdate.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_updateProfileFragment)
+        }
+    }
+}
